14018054a81eb8cb03da084e51a180f0279475db
dudirhd <dmitriy-ermolenko-2002@mail.ru>
2024-10-05 15:49:26 +0500

    New Format + handleOpenAside

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,29 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
+import { ArrowButton } from 'src/ui/arrow-button';
+import { Button } from 'src/ui/button';
 
+import { useState } from 'react';
+
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpened, setIsOpened] = useState(false);
+	// const isOpened = false;
+	const className = clsx(styles.container, {
+		[styles.container_open]: isOpened,
+	});
+
+	const handleClick = () => {
+		setIsOpened((isOpened) => !isOpened);
+	};
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpened} onClick={handleClick} />
+			<aside className={className}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
